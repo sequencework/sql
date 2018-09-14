5e7e698d2a72c92eca572ab0bd1d6e12f947eac8
luc <luc.leray@gmail.com>
2018-09-14 13:00:13 +0200

    Add helpers functions to `sql(db)`

--- pg.js
+++ pg.js
@@ -1,8 +1,24 @@
 const sql = require('./sql')
 
-const sqlPG = db => async (...args) => {
-  const { rows } = await db.query(sql(...args))
-  return rows
-}
+module.exports = db => {
+  const fn = (...args) => db.query(sql(...args))
 
-module.exports = sqlPG
+  fn.one = async (...args) => {
+    const {
+      rows: [row]
+    } = await db.query(sql(...args))
+    return row
+  }
+
+  fn.many = async (...args) => {
+    const { rows } = await db.query(sql(...args))
+    return rows
+  }
+
+  fn.count = async (...args) => {
+    const { rowCount } = await db.query(sql(...args))
+    return rowCount
+  }
+
+  return fn
+}
