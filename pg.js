63d85b1fc982573612f84d60888c2c0a325db569
luc <luc.leray@gmail.com>
2018-09-14 13:13:57 +0200

    replace sql(db).one by sql.one(db)

--- pg.js
+++ pg.js
@@ -1,24 +1,20 @@
 const sql = require('./sql')
 
-module.exports = db => {
-  const fn = (...args) => db.query(sql(...args))
+module.exports = db => (...args) => db.query(sql(...args))
 
-  fn.one = async (...args) => {
-    const {
-      rows: [row]
-    } = await db.query(sql(...args))
-    return row
-  }
+module.exports.one = db => async (...args) => {
+  const {
+    rows: [row]
+  } = await db.query(sql(...args))
+  return row
+}
 
-  fn.many = async (...args) => {
-    const { rows } = await db.query(sql(...args))
-    return rows
-  }
+module.exports.many = db => async (...args) => {
+  const { rows } = await db.query(sql(...args))
+  return rows
+}
 
-  fn.count = async (...args) => {
-    const { rowCount } = await db.query(sql(...args))
-    return rowCount
-  }
-
-  return fn
+module.exports.count = db => async (...args) => {
+  const { rowCount } = await db.query(sql(...args))
+  return rowCount
 }
